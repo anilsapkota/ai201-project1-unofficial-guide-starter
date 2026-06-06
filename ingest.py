bef74565cad94c30fb175c3861161f9254318666
Anil Sapkota <muzicmaniac2015@gmail.com>
2026-06-06 13:04:58 -0500

    added a section to remove the table of content from it

--- ingest.py
+++ ingest.py
@@ -11,7 +11,12 @@ def fetch_and_clean(url:str) ->str:
     for tag in soup(["nav", "header","footer","script","style"]):
         tag.decompose()
     
-    
+    #Remove the table of content 
+    for ul in soup.find_all("ul"):
+        links = ul.find_all("a",href=True)
+        if links and all(a["href"].startswith("#") for a in links):
+            ul.decompose()
+            
     #Get the main content area
     main = soup.find("main") or soup.find("article") or soup.find("body")
 
